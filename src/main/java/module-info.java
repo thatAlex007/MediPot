0245c37de6c20392cfb00ce4d7e370607f089a7b
thatAlex007 <alexander.apostol9@gmail.com>
2025-04-08 15:06:57 +0200

    medi

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module at.spengergasse.medipot {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens at.spengergasse.medipot to javafx.fxml;
+    exports at.spengergasse.medipot;
+}
